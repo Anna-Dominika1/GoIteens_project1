3979af2cf1bb1ddc5b77ad0be872bedd4c84337e
Igoryakib <Igoryakib@gmail.com>
2021-09-19 13:28:23 +0300

    added new file

---
+++ src/apiFunctions.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'bfa52d7e3cb8b977a558f7a3657e1861';
+
+
+
+
+
+export {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import { error, success} from '@pnotify/core';
+import { defaults } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+defaults.delay = 5000;
+const debounce = require('lodash.debounce');
+import * as apiFunctions from 'apiFunctions.js';
