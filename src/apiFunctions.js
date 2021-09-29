150609e348e77323e9261c766e42d2cdfdb5855b
Igoryakib <Igoryakib@gmail.com>
2021-09-29 23:18:43 +0300

    added main

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,25 +1,44 @@
-const axios = require('axios').default; 
-let pageNumber = 1;
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-axios.defaults.params = {
-    api_key: 'bfa52d7e3cb8b977a558f7a3657e1861',
-    language: 'uk-UA',
-    page: pageNumber,
-    include_image_language: 'ua',
-}
 
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'bfa52d7e3cb8b977a558f7a3657e1861';
+
+let pageNumber = 1;
+let checked;
 
 const fetchFilmsFn = (searchQuery) => {
     if (searchQuery === '') {
         return;
     }
     pageNumber = 1;
-    axios(`/search/movie?query=${searchQuery}`)
-    .then(res => console.log(res.data))
+    checked = true;
+   return fetch(`${BASE_URL}/search/movie?query=${searchQuery}&api_key=${API_KEY}&language=uk-UA&page=1&include_image_language=ua&page=${pageNumber}`)
+    .then(res => res.json())
     .catch(err => {
         console.warn(err)
     })
 };
 
+const defaultFetchFilm = () => {
+    pageNumber = 1;
+   return fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=uk-UA&page=1&include_image_language=ua&page=${pageNumber}`).then(res => res.json())
+        .catch(error => console.warn(error))
 
-export {fetchFilmsFn};
+}
+
+
+const paginationFetchFilms = (searchQuery) => {
+    pageNumber += 1;
+    if (checked) {
+        return fetch(`${BASE_URL}/search/movie?query=${searchQuery}&api_key=${API_KEY}&language=uk-UA&page=1&include_image_language=ua&page=${pageNumber}`)
+    .then(res => res.json())
+    .catch(err => {
+        console.warn(err)
+    })
+    }
+    checked = false;
+    return fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=uk-UA&page=1&include_image_language=ua&page=${pageNumber}`).then(res => res.json())
+        .catch(error => console.log(error))
+}
+
+
+export {fetchFilmsFn, defaultFetchFilm, paginationFetchFilms};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
+import './checkbox.js';
 import { error, success} from '@pnotify/core';
 import { defaults } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
@@ -8,6 +9,34 @@ import './apiFunctions';
 defaults.delay = 5000;
 const debounce = require('lodash.debounce');
 import * as apiFunctions from './apiFunctions.js';
+import cardTemplate from './templates/cardTemplate.hbs';
+
+const getRefs = {
+  btnLoadMore: document.querySelector('.js-load_more'),
+  body: document.querySelector('body'),
+  cardContainer: document.querySelector('.js-card-container'),
+  rating: document.querySelector('.card-menu-rating'),
+  card: document.querySelector('.card')
+};
+
+const verifyDataLength = (array) => {
+  if (array.length >= 0) { 
+    console.log('f');
+   success({text: 'Знайдено декілька фільмів :)!!'});
+   return array;
+  }
+  if (array.length === 0) {
+    error({text: 'На жаль ми нічого не знайшли :('});
+        return
+  }
+};
+
+const rendersFilm = (markups) => {
+  const makup = cardTemplate(markups);
+  getRefs.cardContainer.insertAdjacentHTML('beforeend', makup);
+}
+
+apiFunctions.defaultFetchFilm().then(res => rendersFilm(res));
 
 const input = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
@@ -17,11 +46,26 @@ const expand = () => {
   input.classList.toggle("square");
 };
 
-const inputFn = (event) => {
-    apiFunctions.fetchFilmsFn(event.target.value);
+
+
+const inputFn = async (event) => {
+    getRefs.cardContainer.innerHTML = '';
+   const result = await apiFunctions.fetchFilmsFn(event.target.value);
+   verifyDataLength(result);
+   rendersFilm(result);
 };
 
 searchBtn.addEventListener("click", expand);
 input.addEventListener('input', debounce((event) => {
   inputFn(event);
 }, 650));
+
+const loadMoreBtn = async () => {
+ await apiFunctions.paginationFetchFilms(document.querySelector('.input').value).then(rendersFilm);
+  const element = document.querySelector('.films_section');
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
+getRefs.btnLoadMore.addEventListener('click', loadMoreBtn)
